44c915de6d5922b78bed45a70d1818850a7aa0b4
Priyabrata <priyabrata8558@gmail.com>
2024-04-01 08:46:30 +0530

    Refactor user.controller.js: Add getWatchHistory function and fix typo in video.model.js

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -29,7 +29,7 @@ const videoSchema = new Schema({
         type:Boolean,
         default:true,
     },
-    ower:{
+    owner:{
         type:Schema.Types.ObjectId,
         ref:"User",
     }
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
+import { changeCurrentPassword, getCurrentUser, getUserChannelProfile, getWatchHistory, loginUser, logoutUser, refreshAccessToken, registerUser, updateAccountDetails, updateAvatar, updateCoverImage } from "../controllers/user.controller.js";
 import upload from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -22,5 +22,12 @@ router.route("/login").post(loginUser)
 //secured route
 router.route("/logout").post(verifyJWT , logoutUser)
 router.route("/refreshtoken").post( refreshAccessToken )
+router.route("/change-password").post(verifyJWT , changeCurrentPassword)
+router.route("/current-user").get(verifyJWT , getCurrentUser)
+router.route("/update-profile").patch(verifyJWT , updateAccountDetails)
+router.route("/update-avatar").patch(verifyJWT , upload.single("avatar"), updateAvatar)
+router.route("/update-cover-image").patch(verifyJWT , upload.single("coverImage"), updateCoverImage)
+router.route("/channel/:username").get(verifyJWT , getUserChannelProfile)
+router.route("/history").get(verifyJWT , getWatchHistory)
 
 export default router
